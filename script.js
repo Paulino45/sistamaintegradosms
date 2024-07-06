372642cdd950e852942c088ad7f32c530b932362
Paulino <122178422+Paulino45@users.noreply.github.com>
2024-07-06 19:04:44 -0300

    Update script.js

--- script.js
+++ script.js
@@ -104,3 +104,14 @@ function sairDoSistema() {
     // Redireciona para a página inicial
     window.location.href = "index.html";
 }
+ // Mascara do CPF 000.000.000-00
+document.getElementById('cpf').addEventListener('input', function(e) {
+    var value = e.target.value;
+    var cpfPattern = value.replace(/\D/g, '') // Remove qualquer coisa que não seja número
+                          .replace(/(\d{3})(\d)/, '$1.$2') // Adiciona ponto após o terceiro dígito
+                          .replace(/(\d{3})(\d)/, '$1.$2') // Adiciona ponto após o sexto dígito
+                          .replace(/(\d{3})(\d)/, '$1-$2') // Adiciona traço após o nono dígito
+                          .replace(/(-\d{2})\d+?$/, '$1'); // Impede entrada de mais de 11 dígitos
+    e.target.value = cpfPattern;
+  });
+    
